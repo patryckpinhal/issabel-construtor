78a58b754194dd1b6906c501a2411afb55be9215
Patryck Pinhal <48493265+patryckpinhal@users.noreply.github.com>
2023-08-24 17:34:42 -0300

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -8,6 +8,9 @@ iptables -F
 echo Permitindo IP $int1
 iptables -A INPUT -s $int1 -j ACCEPT
 
+echo Liberando ping prtg
+iptables -A INPUT -p icmp -s 10.100.22.160/28 -j ACCEPT
+
 echo Bloqueando qualquer requisição na porta $int2
 iptables -A INPUT -p tcp --dport $int2 -j DROP
 iptables -A INPUT -p udp --dport $int2 -j DROP
@@ -15,9 +18,6 @@ iptables -A INPUT -p udp --dport $int2 -j DROP
 echo Bloquendo qualquer requisição na porta 5060
 iptables -A INPUT -p tcp --dport 5060 -j DROP
 iptables -A INPUT -p udp --dport 5060 -j DROP
-
-echo Liberando ping prtg
-iptables -A INPUT -p icmp -s 10.100.22.160/28 -j ACCEPT
 
 echo Bloqueando ping
 iptables -A INPUT -p icmp -j DROP
