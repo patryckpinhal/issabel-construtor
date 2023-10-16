710be7a402442aa101c286110d80fae4204dea8f
Patryck Pinhal <48493265+patryckpinhal@users.noreply.github.com>
2023-10-16 11:49:48 -0300

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -5,17 +5,17 @@ while true; do
 
 read -p "# Digite o IP utilizado para acesso: " int1
 read -p "# Digite a Porta SSH utilizada: " int2
-read -p "# Digite o IP do servidor SNMP 01: " int3
-read -p "# Digite o IP do servidor SNMP 02: " int5
+read -p "# Digite o IP do servidor SNMP: " int3
 read -p "# Digite o IP do servidor Antivirus: " int4
+read -p "# Digite o IP do servidor Ansible: " int5
 
 clear
 echo " "
 echo " - IP utilizado para acesso...= $int1 "
 echo " - Porta SSH utilizada........= $int2 "
-echo " - IP servidor SNMP 01........= $int3 "
-echo " - IP servidor SNMP 02........= $int5 "
+echo " - IP servidor SNMP...........= $int3 "
 echo " - IP servidor Antivirus......= $int4 "
+echo " - IP servidor Ansible........= $int5 "
 echo " "
 read -p "Caso as informações estejam corretas, digite 'sim', caso deseja corrigir, digite 'nao': " int66
 
@@ -31,27 +31,44 @@ read -p "Caso as informações estejam corretas, digite 'sim', caso deseja corri
 done
 
 iptables -F
+
+#Acessos VPN
 echo Permitindo o IP de acesso: $int1
 iptables -A INPUT -s $int1 -j ACCEPT
 
-echo Liberando ping prtg
-iptables -A INPUT -p icmp -s $int3 -j ACCEPT
-iptables -A INPUT -p icmp -s $int5 -j ACCEPT
+#Acesso Ansible
+echo Permitindo IP do servidor Ansible: $int5
+iptables -A INPUT -s $int5 -p tcp --dport 22000 -j ACCEPT
 
+#Ping PRTG
+
+echo Liberando ping prtg $int3
+iptables -A INPUT -p icmp -s $int3 -j ACCEPT
+
+#Acessos e ping Antivirus
 iptables -A INPUT -s $int4 -p tcp --dport 16761 -j ACCEPT
 iptables -A INPUT -s $int4 -p udp --dport 16761 -j ACCEPT
 iptables -A INPUT -p icmp -s $int4 -j ACCEPT
 
-echo Bloqueando qualquer requisição na porta $int2
-iptables -A INPUT -p tcp --dport $int2 -j DROP
-iptables -A INPUT -p udp --dport $int2 -j DROP
+#Liberação localhost
+iptables -A INPUT -s 127.0.0.1 -j ACCEPT
 
-echo Bloquendo qualquer requisição na porta 5060
-iptables -A INPUT -p tcp --dport 5060 -j DROP
-iptables -A INPUT -p udp --dport 5060 -j DROP
+#Bloqueio portas
+iptables -A INPUT -p tcp --dport $int2 -j DROP      #porta ssh tcp
+iptables -A INPUT -p udp --dport $int2 -j DROP      #porta ssh udp
+iptables -A INPUT -p tcp --dport 5060 -j DROP       #porta sip tcp
+iptables -A INPUT -p udp --dport 5060 -j DROP       #porta sip udp
+iptables -A INPUT -p tcp --dport 3306 -j DROP       #porta mysql tcp
+iptables -A INPUT -p udp --dport 3306 -j DROP       #porta mysql udp
+iptables -A INPUT -p tcp --dport 20005 -j DROP      #porta open webnet tcp
+iptables -A INPUT -p udp --dport 20005 -j DROP      #porta open webnet udp
+iptables -A INPUT -p tcp --dport 8088 -j DROP       #porta web socket tcp
+iptables -A INPUT -p udp --dport 8088 -j DROP       #porta web socket udp
 
+#Bloqueio ping
 echo Bloqueando ping
 iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
 
+#Liberando porta 5060
 echo Liberando range de ips utilizados por clientes
 wget -O - https://raw.githubusercontent.com/patryckpinhal/issabel-construtor/main/ips-liberados.sh | bash
